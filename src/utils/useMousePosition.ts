d2251986b9b5903a803ab931499a66f93f4e0c58
Morgan Galvin <morgan@ludwigthepig.com>
2020-12-17 19:26:14 -0700

    eslint

--- src/utils/useMousePosition.ts
+++ src/utils/useMousePosition.ts
@@ -1,20 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export const useMousePosition = () => {
   const [position, setPosition] = useState({ xPos: 0, yPos: 0 });
 
   useEffect(() => {
-    const setFromMouseEvent = (e: MouseEvent) => setPosition({ xPos: e.clientX, yPos: e.clientY });
-    const setFromTouchEvent = ({touches}: TouchEvent) => setPosition({ xPos: touches[0].clientX, yPos: touches[0].clientY });
-    
-    window.addEventListener("mousemove", setFromMouseEvent);
+    const setFromMouseEvent = (e: MouseEvent) => setPosition({
+      xPos: e.clientX,
+      yPos: e.clientY,
+    });
+    const setFromTouchEvent = ({ touches }: TouchEvent) => setPosition({
+      xPos: touches[0].clientX,
+      yPos: touches[0].clientY,
+    });
+
+    window.addEventListener('mousemove', setFromMouseEvent);
     window.addEventListener('touchmove', setFromTouchEvent, false);
 
     return () => {
-      window.removeEventListener("mousemove", setFromMouseEvent);
+      window.removeEventListener('mousemove', setFromMouseEvent);
       window.removeEventListener('touchmove', setFromTouchEvent, false);
     };
   }, []);
 
-  return {position};
+  return { position };
 };
